b694abc6c6464c43728c9ccf28cda67caea7d7f7
Vasilis Riggs <35762809+vasilisriggs@users.noreply.github.com>
2021-03-31 00:03:52 +0300

    Update TreeFile.java

--- RangeQuery/BPlusTree/src/main/java/ds/bplus/mdpf/TreeFile.java
+++ RangeQuery/BPlusTree/src/main/java/ds/bplus/mdpf/TreeFile.java
@@ -1,34 +1,32 @@
 package ds.bplus.mdpf;
-
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-
 import ds.bplus.bptree.BPlusConfiguration;
 import ds.bplus.bptree.BPlusTree;
 import ds.bplus.bptree.BPlusTreePerformanceCounter;
+import ds.bplus.object.*;
 import ds.bplus.util.InvalidBTreeStateException;
-
 public class TreeFile {
-	
+	private String fileNameBody;
 	private String directory = "DataDirectory/";
-	private String category; //cluser,gaussian,real,uniform
-	private String statFolder = "statistics/";
+	private String category; //cluser,uniform,ais
 	private String treeFolder = "bins/";
-	private String indexingFolder = "indexed/";
 	private String treePathName;
-	private String indexingPathName;
-	private String statPathName;
+		
+	private String readFile;
+	private String writeFilePathC;
+	private String writeFilePathZ;
 	
-	private String filenameC;
-	private String filenameZ;
+	private String fileNameC;
+	private String fileNameZ;
 	
+	private String cCurve = "C";
+	private String zCurve = "Z";
 	
-	private long timeC = 0L;
-	private long timeZ = 0L;
+	private CreateComponentsObject ccoC;
+	private CreateComponentsObject ccoZ;
 	
 	private double minX;
 	private double maxX;
@@ -38,60 +36,41 @@ public class TreeFile {
 	private BPlusTree bpc;
 	private BPlusTree bpz;
 	private BPlusConfiguration bConf;
-	private BPlusTreePerformanceCounter bPerf;
+	private BPlusTreePerformanceCounter bPerfC;
+	private BPlusTreePerformanceCounter bPerfZ;
 	private IndexingFile inf;
 	
 	private int pages;
-	private boolean binariesExisted;
+	private boolean binariesExisted = false;
 	
 	public TreeFile(int pageSize, int keySize, int entrySize, IndexingFile inf) throws IOException, InvalidBTreeStateException{
 		
-		String readMode = "rw+";
 		this.bConf = new BPlusConfiguration(pageSize,keySize,entrySize);
-		this.bPerf = new BPlusTreePerformanceCounter(true);
+		this.bPerfC = new BPlusTreePerformanceCounter(true);
+		this.bPerfZ = new BPlusTreePerformanceCounter(true);
 		this.inf = inf;
 		
-		category = inf.getIndexingCategory();
-		treePathName = directory+category+treeFolder;
-		indexingPathName = directory+category+indexingFolder;
-		statPathName = directory+category+statFolder;
+		setParams();
+		setFileParams(pageSize,keySize,entrySize);		
+		loadTreeFiles();
 		
-		String readFile = inf.getIndexingFileName(); // 10K_8_indexed.txt or 10K_1_8_indexed.txt
-		String[] splitFile = readFile.split("\\."); 
-		filenameC = "Tree_"+splitFile[0]+"_"+String.valueOf(pageSize)+"-"+String.valueOf(keySize+"-"+String.valueOf(entrySize))+"_C.bin"; 													
-		filenameZ = "Tree_"+splitFile[0]+"_"+String.valueOf(pageSize)+"-"+String.valueOf(keySize+"-"+String.valueOf(entrySize))+"_Z.bin";
-		pages = inf.getPages();
-		
-		String writeFileC = treePathName+filenameC;
-		String writeFileZ = treePathName+filenameZ;
-		
-		File fc = new File(writeFileC);
-		File fz = new File(writeFileZ);
-		
-		if(fz.exists() && fc.exists()) {
-			System.out.println("Tree files already exist. Reading for values.");
-			readMode = "rw";
-			bpc = new BPlusTree(bConf,readMode,writeFileC,bPerf);
-			bpz = new BPlusTree(bConf,readMode,writeFileZ,bPerf);
-			minX = inf.getDataObject().getMinLongitude();
-			maxX = inf.getDataObject().getMaxLongitude();
-			minY = inf.getDataObject().getMinLatitude();
-			maxY = inf.getDataObject().getMaxLatitude();
-			binariesExisted = true;
-			return;
+		if(!binariesExisted()) {
+			writeTreeFiles(pageSize,keySize,entrySize);
+			getCreationStatistics();
 		}
+	}
+	private void writeTreeFiles(int pageSize, int keySize, int entrySize) throws IOException, InvalidBTreeStateException {
 		
-		binariesExisted = false;
-		
+		String readMode = "rw+";
 		System.out.println("Initializing TreeFile file. Inserting...");
-		BufferedReader br = new BufferedReader(new FileReader(indexingPathName+readFile));
+		BufferedReader br = new BufferedReader(new FileReader(readFile));
 		
-		bpc = new BPlusTree(bConf,readMode,writeFileC,bPerf);
-		bpz = new BPlusTree(bConf,readMode,writeFileZ,bPerf);
+		bpc = new BPlusTree(bConf,readMode,writeFilePathC,bPerfC);
+		bpz = new BPlusTree(bConf,readMode,writeFilePathZ,bPerfZ);
 		
+		bpc.setCurveMethod(cCurve);
+		bpz.setCurveMethod(zCurve);
 		
-		long start;
-		long end;
 		String entry;
 		long keyC;
 		long keyZ;
@@ -105,99 +84,102 @@ public class TreeFile {
 		minmax = line.split(" ");
 		minX = Double.parseDouble(minmax[0]);
 		maxX = Double.parseDouble(minmax[1]);
+		
 		line = br.readLine(); 
 		minmax = line.split(" ");
 		minY = Double.parseDouble(minmax[0]);
 		maxY = Double.parseDouble(minmax[1]);
 		
-		String nodePrefix = "Key-Value-InsertionTime"; // -2K_1_16_1024-8-24_C.txt . etc.
-		
-		String writeTimeC = statPathName+nodePrefix+"-"+splitFile[0]+"_"+String.valueOf(pageSize)+"-"+String.valueOf(keySize)+"-"+String.valueOf(entrySize)+"_C.txt";
-		String writeTimeZ = statPathName+nodePrefix+"-"+splitFile[0]+"_"+String.valueOf(pageSize)+"-"+String.valueOf(keySize)+"-"+String.valueOf(entrySize)+"_Z.txt";
-		
-		BufferedWriter writerC = new BufferedWriter(new FileWriter(writeTimeC, true));
-		BufferedWriter writerZ = new BufferedWriter(new FileWriter(writeTimeZ, true));
-		
-		int cnt = 0;
 		while((line = br.readLine()) != null) {
-			
-			cnt++;
-			System.out.println(cnt);
 			datas = line.split(" ");
 			entry = datas[0]+","+datas[1];
-			
 			keyC = Long.parseLong(datas[2],2);
 			keyZ = Long.parseLong(datas[3],2);
 			
-			start = System.currentTimeMillis();
-			bpc.insertKey(keyC, entry, duplicates);
-			end = System.currentTimeMillis();
-			timeC = timeC + (end-start);
-			
-			writerC.append(String.valueOf(end-start));
-			writerC.newLine();
-			
-			start = System.currentTimeMillis();
-			bpz.insertKey(keyZ, entry, duplicates);
-			end = System.currentTimeMillis();
-			timeZ = timeZ + (end-start);
-			
-			writerZ.append(String.valueOf(end-start));
-			writerZ.newLine();
+			bpc.insertKey(keyC, entry, duplicates);		
+			bpz.insertKey(keyZ, entry, duplicates);			
 		}
-		
-		writerZ.close();
-		writerC.close();
-		
 		bpc.commitTree();
 		bpz.commitTree();
-		System.out.println("Insertion and TreeFiles files:"+filenameC+" "+filenameZ+" were created with success.");
-		bpc.printCurrentConfiguration();
+		System.out.println("Tree Files: "+fileNameC+", "+fileNameZ+" were created successfully.");
 		br.close();
 	}
-	
+	private void setFileParams(int pageSize, int keySize, int entrySize) {
+		readFile = inf.getIndexingFileName();
+		String[] splitFile = readFile.split("\\."); 
+		readFile = inf.getIndexingPathName()+readFile;
+		fileNameBody = splitFile[0];
+		fileNameC = "Tree_"+fileNameBody+"_"+String.valueOf(pageSize)+"-"+String.valueOf(keySize+"-"+String.valueOf(entrySize))+"_C.bin";
+		fileNameZ = "Tree_"+fileNameBody+"_"+String.valueOf(pageSize)+"-"+String.valueOf(keySize+"-"+String.valueOf(entrySize))+"_Z.bin";
+		writeFilePathC = treePathName+fileNameC; 													
+		writeFilePathZ = treePathName+fileNameZ;
+	}
+	private void loadTreeFiles() throws IOException, InvalidBTreeStateException {
+		File fc = new File(writeFilePathC);
+		File fz = new File(writeFilePathZ);
+		
+		if(fz.exists() && fc.exists()) {
+			System.out.println("Tree Files already exist. Loading Files...");
+			String readMode = "rw";
+			bpc = new BPlusTree(bConf,readMode,writeFilePathC,bPerfC);
+			bpz = new BPlusTree(bConf,readMode,writeFilePathZ,bPerfZ);
+			minX = inf.getDataObject().getMinLongitude();
+			maxX = inf.getDataObject().getMaxLongitude();
+			minY = inf.getDataObject().getMinLatitude();
+			maxY = inf.getDataObject().getMaxLatitude();
+			binariesExisted = true;
+		}
+	}
+	private void setParams() {
+		category = inf.getIndexingCategory()+"/";
+		treePathName = directory+category+treeFolder;
+		pages = inf.getPages();
+	}
+	public int getElements() 
+	{return getIndexingObject().getDataObject().getElements();}
 	public BPlusConfiguration getBConf() 
 	{return bConf;}
-	
-	public BPlusTreePerformanceCounter getBPerf() 
-	{return bPerf;}
-	
+	public BPlusTreePerformanceCounter getBPerfC()
+	{return bPerfC;}
+	public BPlusTreePerformanceCounter getBPerfZ() 
+	{return bPerfZ;}
 	public boolean binariesExisted() 
 	{return binariesExisted;}
-	
 	public BPlusTree getBTreeC()
 	{return bpc;}
-	
 	public BPlusTree getBTreeZ()
 	{return bpz;}
-	
 	public double getMinX() 
 	{return minX;}
-	
 	public double getMaxX() 
 	{return maxX;}
-	
 	public double getMinY() 
 	{return minY;}
-	
 	public double getMaxY() 
 	{return maxY;}
-	
 	public int getPages() 
 	{return pages;}
-	
 	public String getFileNameC() 
-	{return filenameC;}
-	
+	{return fileNameC;}
 	public String getFileNameZ() 
-	{return filenameZ;}
-	
+	{return fileNameZ;}
 	public IndexingFile getIndexingObject() 
 	{return inf;}
-	
-	public long returnTimeC() 
-	{return timeC;}
-	
-	public long returnTimeZ() 
-	{return timeZ;}
+	private void getCreationStatistics() {
+		ccoC = new CreateComponentsObject(fileNameC, this.getElements(), pages, this.bConf.getPageSize(), this.bConf.getKeySize(), this.bConf.getEntrySize(), 
+				bPerfC.getTotalNodeReads(), bPerfC.getTotalInternalNodeReads(), bPerfC.getTotalLeafNodeReads(), bPerfC.getTotalOverflowReads(), 
+				bPerfC.getTotalNodeWrites(), bPerfC.getTotalInternalNodeWrites(), bPerfC.getTotalLeafNodeWrites(), bPerfC.getTotalOverflowWrites(), 
+				bPerfC.getTotalSplits(), bPerfC.getTotalRootSplits(), bPerfC.getTotalInternalNodeSplits(), bPerfC.getTotalLeafSplits(), bPerfC.getTotalPages(), 
+				bPerfC.getTotalOverflowPages(), bPerfC.getTotalInternalNodes(), bPerfC.getTotalLeaves());
+		
+		ccoZ = new CreateComponentsObject(fileNameZ, this.getElements(), pages, this.bConf.getPageSize(), this.bConf.getKeySize(), this.bConf.getEntrySize(), 
+				bPerfZ.getTotalNodeReads(), bPerfZ.getTotalInternalNodeReads(), bPerfZ.getTotalLeafNodeReads(), bPerfZ.getTotalOverflowReads(), 
+				bPerfZ.getTotalNodeWrites(), bPerfZ.getTotalInternalNodeWrites(), bPerfZ.getTotalLeafNodeWrites(), bPerfZ.getTotalOverflowWrites(), 
+				bPerfZ.getTotalSplits(), bPerfZ.getTotalRootSplits(), bPerfZ.getTotalInternalNodeSplits(), bPerfZ.getTotalLeafSplits(), bPerfZ.getTotalPages(), 
+				bPerfZ.getTotalOverflowPages(), bPerfZ.getTotalInternalNodes(), bPerfZ.getTotalLeaves());
+	}
+	public CreateComponentsObject getStatObjectC() 
+	{return ccoC;}
+	public CreateComponentsObject getStatObjectZ() 
+	{return ccoZ;}
 }
